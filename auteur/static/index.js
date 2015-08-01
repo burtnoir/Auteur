34b8e872ce2b1e341d1570398a71192c15ad16cd
burtnoir <burtnoir@gmail.com>
2015-08-01 15:26:59 -0400

    Improve the indentation

--- auteur/static/index.js
+++ auteur/static/index.js
@@ -1,32 +1,32 @@
 $(document)
-	.ready(
-		function() {
+    .ready(
+        function() {
 
-		    /**
-		     * Go to the server to update the project list depending on
-		     * whether we are showing the templates or not.
-		     */
-		    window.templateFilter = function(is_template) {
+    /**
+    * Go to the server to update the project list depending on
+    * whether we are showing the templates or not.
+    */
+    window.templateFilter = function(is_template) {
 
-			// Post the data to be saved and notify the user when
-			// it's
-			// done.
-			$('#projectlist > a')
-				.each(
-					function(i) {
-					    var el = $(this);
-					    if ((is_template && el
-						    .data('is_template') === 'True')
-						    || (!is_template && el
-							    .data('is_template') === 'False')) {
-						el.show();
-					    } else {
-						el.hide();
-					    }
-					});
-		    };
+    // Post the data to be saved and notify the user when
+    // it's
+    // done.
+    $('#projectlist > a')
+        .each(
+            function(i) {
+                var el = $(this);
+                if ((is_template && el
+                    .data('is_template') === 'True')
+                    || (!is_template && el
+                        .data('is_template') === 'False')) {
+                el.show();
+                } else {
+                    el.hide();
+                }
+            });
+};
 
-		    // Initial filter.
-		    window.templateFilter(false);
+    // Initial filter.
+    window.templateFilter(false);
 
-		});
+    });
