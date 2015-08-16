71c2326e8b4b497cfd6c71ea9c63bbd7c230a49a
burtnoir <burtnoir@gmail.com>
2015-08-16 14:10:37 -0400

    Additional multi language stuff.  It turns out we need to use the lazy gettext for the WTForms. Also added a missing French translation.

--- auteur/forms.py
+++ auteur/forms.py
@@ -9,20 +9,20 @@ from auteur.database import db_session
 from auteur.models import Project
 from wtforms.validators import ValidationError
 from sqlalchemy.sql.functions import func
-from flask_babel import gettext
+from flask_babel import gettext, lazy_gettext
 
 class ProjectForm(Form):
-    name = StringField(gettext('Name'),
+    name = StringField(lazy_gettext('Name'),
                              [
-                              validators.Length(min=1, max=256, message=gettext('This name is too long - 256 characters should be enough for anyone.')),
-                              validators.DataRequired(message=gettext('You need to name the project.'))
+                              validators.Length(min=1, max=256, message=lazy_gettext('This name is too long - 256 characters should be enough for anyone.')),
+                              validators.DataRequired(message=lazy_gettext('You need to name the project.'))
                               ])
-    description = TextAreaField(gettext('Description'),
+    description = TextAreaField(lazy_gettext('Description'),
                          [
-                          validators.DataRequired(message=gettext('You need to say something about the project.'))
+                          validators.DataRequired(message=lazy_gettext('You need to say something about the project.'))
                           ])
-    template = SelectField(gettext('Template'), coerce=int)
-    is_template = BooleanField(gettext('Project Is a Template?'))
+    template = SelectField(lazy_gettext('Template'), coerce=int)
+    is_template = BooleanField(lazy_gettext('Project Is a Template?'))
     
     def validate_project_name(form, field):
         '''
@@ -31,4 +31,4 @@ class ProjectForm(Form):
         '''
         num_same_names = db_session.query(Project.name, func.count(Project.name)).filter_by(name=field.data).scalar()
         if num_same_names > 0:
-            raise ValidationError(gettext('Name already used.  Maybe a writer should try to be more original?'))
+            raise ValidationError(lazy_gettext('Name already used.  Maybe a writer should try to be more original?'))
